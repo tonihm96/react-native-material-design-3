3ab16014a60ce20e21277698ef8b54e7dadb8fd0
Antonio Henrique Macan <toninhm96@gmail.com>
2026-01-03 12:01:44 -0300

    add useStateLayers hook for managing state layer colors

---
+++ src/hooks/use-state-layers.ts
@@ -0,0 +1,25 @@
+import { useMemo } from 'react';
+
+import { useTheme } from '@/contexts/theme';
+import { Colord } from 'colord';
+
+export function useStateLayers(color: string) {
+  const theme = useTheme();
+  const colorParser = useMemo(() => new Colord(color), [color]);
+
+  return useMemo(
+    () => ({
+      hover: colorParser.alpha(theme.stateLayers.hover).toHex(),
+      focus: colorParser.alpha(theme.stateLayers.focus).toHex(),
+      press: colorParser.alpha(theme.stateLayers.press).toHex(),
+      drag: colorParser.alpha(theme.stateLayers.drag).toHex(),
+    }),
+    [
+      colorParser,
+      theme.stateLayers.drag,
+      theme.stateLayers.focus,
+      theme.stateLayers.hover,
+      theme.stateLayers.press,
+    ]
+  );
+}
