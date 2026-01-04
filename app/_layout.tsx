81b386e0f1f2fe521e0f915a2a767dd4bb0ef41a
Antonio Henrique Macan <toninhm96@gmail.com>
2026-01-04 11:29:30 -0300

    update RootLayout to use color scheme for theme selection

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,17 @@
 import { Slot } from 'expo-router';
+import { useColorScheme } from 'react-native';
 
 import { ThemeProvider } from '@/contexts/theme';
+import { materialDesign3DefaultDarkTheme, materialDesign3DefaultLightTheme } from '@/theme/default';
 
 export default function RootLayout() {
+  const scheme = useColorScheme();
+  const isDark = scheme === 'dark';
+
   return (
-    <ThemeProvider>
+    <ThemeProvider
+      theme={isDark ? materialDesign3DefaultDarkTheme : materialDesign3DefaultLightTheme}
+    >
       <Slot />
     </ThemeProvider>
   );
