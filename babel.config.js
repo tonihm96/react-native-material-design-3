2ac313c5551ea976bbeefe4e19019058a811100d
Antonio Henrique Macan <toninhm96@gmail.com>
2026-01-11 21:00:25 -0300

    standardize quotes in babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,11 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: ['babel-preset-expo'],
     plugins: [
-      ["babel-plugin-react-docgen-typescript", { exclude: "node_modules" }],
-      "@babel/plugin-transform-class-static-block",
-      "react-native-worklets/plugin",
+      ['babel-plugin-react-docgen-typescript', { exclude: 'node_modules' }],
+      '@babel/plugin-transform-class-static-block',
+      'react-native-worklets/plugin',
     ],
   };
 };
