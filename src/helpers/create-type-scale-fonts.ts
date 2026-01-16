e6f8984e7ad5a22b5610944e74db0f5ca0cd90b8
Antonio Henrique Macan <toninhm96@gmail.com>
2026-01-16 01:27:40 -0300

    export createMaterialDesign3TypeScaleFonts as default

--- src/helpers/create-type-scale-fonts.ts
+++ src/helpers/create-type-scale-fonts.ts
@@ -1,8 +1,10 @@
 import { MaterialDesign3TypeScale } from '@/types/typeScale';
 import defaultFonts from '@/theme/default/fonts';
 
-export const createMaterialDesign3TypeScaleFonts = <T extends MaterialDesign3TypeScale>(
+const createMaterialDesign3TypeScaleFonts = <T extends MaterialDesign3TypeScale>(
   setup?: (defaults: MaterialDesign3TypeScale) => T
 ): T => {
   return setup ? setup(defaultFonts) : (defaultFonts as T);
 };
+
+export default createMaterialDesign3TypeScaleFonts;
