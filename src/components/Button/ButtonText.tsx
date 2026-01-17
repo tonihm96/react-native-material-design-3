50f47d2a9404be6574d10cdc072fa065e149e2ac
Antonio Henrique Macan <toninhm96@gmail.com>
2026-01-17 11:54:50 -0300

    update ButtonText imports

--- src/components/Button/ButtonText.tsx
+++ src/components/Button/ButtonText.tsx
@@ -1,47 +1,13 @@
 import { use } from 'react';
 
 import { useTheme } from '@/contexts/theme';
-import { MaterialDesign3Colors } from '@/types/colors';
-import { MaterialDesign3TypeScale } from '@/types/typeScale';
 
 import Text, { TextProps } from '../Text';
-import ButtonContext, { ButtonSize, ButtonVariant } from './ButtonContext';
 
-export interface ButtonTextProps extends TextProps {}
+import { getFontSizeStyle, getVariantTextColor } from './Button.utils';
+import ButtonContext from './ButtonContext';
 
-const getVariantTextColor = (variant: ButtonVariant, colors: MaterialDesign3Colors) => {
-  switch (variant) {
-    case 'elevated':
-      return colors.primary;
-    case 'filled':
-      return colors.onPrimary;
-    case 'tonal':
-      return colors.onSecondaryContainer;
-    case 'outlined':
-      return colors.onSurfaceVariant;
-    case 'text':
-      return colors.primary;
-    default:
-      return colors.onSurface;
-  }
-};
-
-const getFontSizeStyle = (size: ButtonSize, fonts: MaterialDesign3TypeScale) => {
-  switch (size) {
-    case 'xsmall':
-      return fonts.labelLarge;
-    case 'small':
-      return fonts.labelLarge;
-    case 'medium':
-      return fonts.titleMedium;
-    case 'large':
-      return fonts.headlineSmall;
-    case 'xlarge':
-      return fonts.headlineLarge;
-    default:
-      return fonts.labelLarge;
-  }
-};
+export type ButtonTextProps = TextProps;
 
 const ButtonText = ({ children, style, ...props }: ButtonTextProps) => {
   const theme = useTheme();
